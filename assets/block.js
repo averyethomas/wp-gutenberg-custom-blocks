1b483f3813e2a17aa7652976eea7587a4a051b51
Avery Thomas <averyethomas@gmail.com>
2018-04-11 17:53:19 -0400

    editable block working

--- assets/block.js
+++ assets/block.js
@@ -1,22 +1,46 @@
 ( function() {
-    var __ = wp.i18n.__;
-    var createElement = wp.element.createElement;
-    var registerBlockType = wp.blocks.registerBlockType;
+    var __                  = wp.i18n.__;
+    var createElement       = wp.element.createElement;
+    var registerBlockType   = wp.blocks.registerBlockType;
+    var RichText            = wp.blocks.RichText;
 	
     registerBlockType( 'custom-blocks/teaser-block', {
         title: __( 'Teaser Block' ),
-        icon: 'lock',
+        icon: 'unlock',
         category: 'common',
+        
+        attributes: {
+            content: {
+                type: 'array',
+                source: 'children',
+                selector: 'p',
+                ///default: 'Editable block content ...',
+            }
+        },
 
         edit: function( props ) {
+            var content = props.attributes.content;
+            var focus = props.focus;
+            
             return createElement(
-                'p', { className: props.className }, 'Editor.' // Block content
+                RichText, {
+                    tagName: 'p',
+                    className: props.className,
+                    value: content,
+                    onChange: function(input) {
+                        props.setAttributes({ content: input });
+                    },
+                    focus: focus,
+                    onFocs: props.setFocus
+                }
             );
         },
         
         save: function( props ) {
+            var content = props.attributes.content;
+            
             return createElement(
-                'p', { className: props.className }, 'Front End.' // Block content
+                'p', { className: props.className }, content
             );
         },
     });
